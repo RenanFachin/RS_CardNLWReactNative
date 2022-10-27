e0b8ccc7a2626db7c91ac08be9e19459c298d629
Renan <renan.fachin@hotmail.com>
2022-10-27 19:15:11 -0300

    Iniciando a câmera do usuário a partir de uma autorização

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
 import { Image, SafeAreaView, ScrollView, TextInput, View } from 'react-native';
 
 import { Header } from '../components/Header';
@@ -9,7 +10,21 @@ import { styles } from './styles';
 import { POSITIONS, PositionProps } from '../utils/positions';
 
 export function Home() {
+  // Criando um estado para verificar se o usuário deu a permissão para utilizar a câmera
+  const [hasCameraPermission, setHasCameraPermission] = useState(false)
+
   const [positionSelected, setPositionSelected] = useState<PositionProps>(POSITIONS[0]);
+
+
+  useEffect(() => {
+    // Solicitando ao usuário a permissão para utilizar a câmera
+    Camera.requestCameraPermissionsAsync()
+    .then(response => {
+      setHasCameraPermission(response.granted)
+    })
+  }, [])
+
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -19,7 +34,15 @@ export function Home() {
 
           <View style={styles.picture}>
 
-            <Image source={{ uri: 'https://github.com/rodrigorgtic.png' }} style={styles.camera} />
+            {
+              // CASO hasCameraPermission seja true = <Camera />
+              hasCameraPermission ? 
+                <Camera 
+                  style={styles.camera} 
+                /> : 
+              // Caso não tenha a permissão (False) do uso da câmera será utilizada uma imagem default
+              <Image source={{ uri: 'https://images.gutefrage.net/media/fragen/bilder/meine-kamera-auf-windows-10-funktioniert-nicht-was-tun/0_big.jpg?v=1584606917000' }} style={styles.camera} />
+            }
 
             <View style={styles.player}>
               <TextInput
